3907aa9ac5285e6e4f4cec0250027689a56257d8
AndrewDanie <103277927+AndrewDanie@users.noreply.github.com>
2022-07-01 19:41:58 +0300

    add: added 5 new buttons
    fixed uncorrect current time, refactored all the code

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,39 +1,91 @@
-from aiogram import Bot, types
+from aiogram import Bot
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 import requests
-import datetime
-date = datetime.date.today()
+from datetime import timedelta
 
-from config import TELEGRAM_TOKEN, yandex_token, from_code, to_code
+from config import TELEGRAM_TOKEN, yandex_token
+
+home = 'Пет'
+city = 'СПб'
+msg = {
+    '2h_pet': f'Ближайшие 2 часа({home})',
+    '2h_bal': f'Ближайшие 2 часа({city})',
+    'tday_pet': f'Сегодня({home})',
+    'tday_bal': f'Сегодня({city})',
+    'tmr_pet': f'Завтра({home})',
+    'tmr_bal': f'Завтра({city})',
+}
 
 
-def get_subtrain_data(from_station, to_station, date, next_hours=2):
-    current_time = str(datetime.datetime.now().time())[:5]
-    current_minutes = int(current_time[:2])*60 + int(current_time[3:])
+def get_subtrain_data(msg_date, key=msg['2h_pet']):
 
+    segments = get_data_from_yandex_API(key, msg_date)
+    return form_answer(segments, msg_date, key)
+
+
+def get_data_from_yandex_API(key, msg_date):
+    date = get_needed_date(msg_date, key)
+    from_station, to_station = get_stations_id(key)
     req = requests.get(f'https://api.rasp.yandex.net/v3.0/search/?'
-                            f'apikey={yandex_token}'
-                            f'&from={from_station}'
-                            f'&to={to_station}'
-                            f'&date={date}'
-                            )
-    print(req.status_code)
-    print(current_time)
-    response_text = req.json()
-    nearby_counter = 0
-    answer = ''
-    segments = response_text['segments']
+                       f'apikey={yandex_token}'
+                       f'&from={from_station}'
+                       f'&to={to_station}'
+                       f'&date={date}'
+                       )
+    return req.json()['segments']
+
+
+def get_stations_id(key):
+    from config import petr_code, balt_code
+    if key == '2h_pet' or key == 'tday_pet' or key == 'tmr_pet':
+        print('from Petergof')
+        return petr_code, balt_code
+    else:
+        print('from SPb')
+        return balt_code, petr_code
+
+
+def get_needed_date(msg_date, key):
+    if key == 'tmr_pet' or key == 'tmr_bal':
+        next_day = timedelta(days=1)
+        return msg_date.date() + next_day
+    else:
+        return msg_date.date()
+
+
+def get_needed_time_period(msg_date, key):
+    if key == '2h_pet' or key == '2h_bal':
+        return 2
+    else:
+        return 24
+
+
+def get_current_minute(msg_date, key):
+    curr_hour = str(msg_date.time())[:5]
+    if key == 'tmr_pet' or key == 'tmr_bal':
+        curr_min = 0
+    else:
+        curr_min = int(curr_hour[:2]) * 60 + int(curr_hour[3:])
+    return curr_min
+
+
+def form_answer(segments, msg_date, key):
+    next_hours = get_needed_time_period(msg_date, key)
+    curr_min = get_current_minute(msg_date, key)
+
+    answer = f'{msg[key]}:\n\n'
     for i in range(len(segments)):
         train = segments[i]
         departure_time = train['departure'][11:16]
         departure_minutes = int(departure_time[:2])*60 + int(departure_time[3:])
         arrival_time = train['arrival'][11:16]
         transport_type = train['thread']['transport_subtype']['title'][:11]
-        if departure_minutes < current_minutes:
+        if departure_minutes < curr_min:
             continue
-        if 0 <= departure_minutes - current_minutes <= next_hours * 60:
+        if 0 <= departure_minutes - curr_min <= next_hours * 60:
             answer += f'{departure_time} {arrival_time} {transport_type}\n'
         else:
             if len(answer) == 0:
@@ -47,8 +99,17 @@ def get_subtrain_data(from_station, to_station, date, next_hours=2):
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dispatcher = Dispatcher(bot)
-button_2hours = types.KeyboardButton('Ближайшие 2 часа')
-keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_2hours)
+
+buttons = {}
+for key in msg:
+    buttons[key] = KeyboardButton(msg[key])
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+
+keyboard = keyboard.row(msg['2h_pet'], msg['2h_bal'])
+keyboard = keyboard.row(msg['tday_pet'], msg['tday_bal'])
+keyboard = keyboard.row(msg['tmr_pet'], msg['tmr_bal'])
+# for key in msg:
+#     keyboard = keyboard.add
 
 
 async def send_msg(id, answr):
@@ -59,24 +120,25 @@ async def send_msg(id, answr):
 
 
 @dispatcher.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Привет! давай посмотрим электрички!',
+async def start(message: Message):
+    await bot.send_message(message.from_user.id,
+                           'Привет! давай посмотрим электрички!\n'
+                           f'({home}) - из Петергофа\n'
+                           f'({city}) - с Балтийского вокзала',
                            reply_markup=keyboard)
-    print('1')
-    answr = get_subtrain_data(from_code, to_code, date)
-    await send_msg(message.from_user.id, answr)
 
 
 @dispatcher.message_handler()
-async def bot_message(message: types.Message):
-    if message.text == 'Ближайшие 2 часа':
-        answr = get_subtrain_data(from_code, to_code, date)
-        await send_msg(message.from_user.id, answr)
-    else:
-        await bot.send_message(message.from_user.id, 'Неизвестная команда')
+async def bot_message(message: Message):
+    answr = None
+    for key in msg:
+        if message.text == msg[key]:
+            answr = get_subtrain_data(message.date, key=key)
+
+    if answr is None:
+        answr ='Неизвестная команда'
+    await send_msg(message.from_user.id, answr)
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher)
-
-
-#somechanges
